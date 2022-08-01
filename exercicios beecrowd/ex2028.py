6283599593e29ac5c2c5998451fbfc0aec332065
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-08-01 20:32:06 -0300

    estudo

--- exercicios beecrowd/ex2028.py
+++ exercicios beecrowd/ex2028.py
@@ -1,24 +1,22 @@
 x = 1
 while True:
-    lista = [0]
-    y = 1
     try:
         n = int(input())
-        for a in range(0, n):
-            for b in range(0, a+1):
-                lista.append(a+1)
-                y += 1
-        if n == 0:
-            print('Caso 1: 1 numero')
-        else:
-            print(f'Caso {x}: {y} numeros')
-        x += 1
-        for c in range(0, len(lista)):
-            if c == len(lista) - 1:
-                print(f'{lista[c]}')
-            else:
-                print(f'{lista[c]}', end=' ')
+        f = [0]
+        for i in range(n + 1):
+            for j in range(i):
+                f.append(i)
+        print(f'Caso {x}: {len(f)} numero', end='')
+        if n != 0:
+            print('s', end='')
         print()
-        lista.clear()
+        for i in range(len(f)):
+            print(f[i], end='')
+            if i != len(f) - 1:
+                print(' ', end='')
+            else:
+                print()
+        print()
+        x += 1
     except EOFError:
         break
