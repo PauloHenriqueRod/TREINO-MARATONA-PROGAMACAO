25e93a7b7162bd165e90576410f5255dfa1f6a01
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-08-01 21:12:14 -0300

    strings

---
+++ exercicios beecrowd/ex 2160.py
@@ -0,0 +1,5 @@
+nome = str(input())
+if nome.count('')-1<=80:
+    print('YES')
+else:
+    print('NO')
---
+++ exercicios beecrowd/ex 2165.py
@@ -0,0 +1,5 @@
+tweet = str(input())
+if (tweet.count('')-1) <= 140:
+    print('TWEET')
+else:
+    print('MUTE')
