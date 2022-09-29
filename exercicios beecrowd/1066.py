2c649eb354b2728c7bae2a4017cfd6bb89b1c6d2
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2022-09-29 18:54:41 -0300

    matemática

--- exercicios beecrowd/1066.py
+++ exercicios beecrowd/1066.py
@@ -1,18 +1,1 @@
-    pares = 0
-    impares = 0
-    pos = 0
-    neg = 0
-    for i in range(0, 5):
-        n = int(input())
-        if n > 0:
-            pos += 1
-        if n < 0:
-            neg += 1
-        if (n % 2) != 0:
-            impares += 1
-        if (n % 2) == 0:
-            pares += 1
-    print(f'{pares} valor(es) par(es)')
-    print(f'{impares} valor(es) impar(es)   ')
-    print(f'{pos} valor(es) positivo(s)')
-    print(f'{neg} valor(es) negativo(s)')
+import numpy as no
---
+++ exercicios beecrowd/ex1805.py
@@ -0,0 +1,3 @@
+x, y = [int(n) for n in input().split()]
+soma = ((x+y)*(y-x+1))/2
+print(int(soma))
