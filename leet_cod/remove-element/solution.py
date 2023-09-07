385e3d1136f723f7fa5328149a83b2edfa4cc273
PauloHenriqueRod <phrod.devp@gmail.com>
2023-09-06 21:28:53 -0300

    remove-element

---
+++ leet_cod/remove-element/solution.py
@@ -0,0 +1,11 @@
+def remove_element(nums: list[int], val: int) ->int:
+    tam = len(nums)
+    nums[:] = [i for i in nums if i !=val]
+    k = len(nums)
+    nums += ['_' for i in range(tam-k)]
+    return k
+lista = [0,1,2,2,3,0,4,2]
+print(remove_element(lista, 2))
+print(lista)
+
+
