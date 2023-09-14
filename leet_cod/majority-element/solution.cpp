dc4da68498a0af0329d4b4c27e0c6122b4be6532
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2023-09-13 22:17:19 -0300

    majority element

---
+++ leet_cod/majority-element/solution.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <string>
+
+int majorityElement(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+    int a = nums1.size();
+    int m = a/2;
+    return nums[m];
+    }
+
+
+int main(){
+    std:: vector<int> lista = {0,0,1,1,1,2,2,3,3,4};
+    int b = majorityElement(lista);
+    cout << b << endl;
+}
--- leet_cod/majority-element/solution.py
+++ leet_cod/majority-element/solution.py
@@ -2,5 +2,3 @@ class Solution:
     def majorityElement(self, nums: list[int]) -> int:
         nums.sort()
         return nums[len(nums)//2]
-
-print('deu certo')
