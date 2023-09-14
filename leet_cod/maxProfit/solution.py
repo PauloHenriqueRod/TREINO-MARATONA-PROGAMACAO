2a2958b636bcc5509d121eab3a0421d642368ffb
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2023-09-13 22:16:19 -0300

    Update solution.py

--- leet_cod/maxProfit/solution.py
+++ leet_cod/maxProfit/solution.py
@@ -1,13 +1,8 @@
 def maxProfit(prices: list[int]) -> int:
-    if not prices:
-        return 0
-    
     max_profit = 0
     min_price = prices[0]
 
     for price in prices:
-        print(min_price)
-        print(max_profit)
         min_price = min(min_price, price)
         max_profit = max(max_profit, price - min_price)
     
