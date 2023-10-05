bca9a037b7e3346bd26b700e1d65f5ee5c6966ab
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2023-10-05 18:39:44 -0300

    melhorando a solução

--- leet_cod/h-index/solution.py
+++ leet_cod/h-index/solution.py
@@ -2,9 +2,9 @@ def hIndex(citations: list[int]) -> int:
     citations.sort()
     tam = len(citations)
     for i in range(0, tam):
-        if citations[i] >= tam-i:
-            print(citations[i])
-            return tam-i
+        x = tam-i
+        if citations[i] >= x:
+            return x
     return 0
 
 print(hIndex([3,0,6,1,5]))
