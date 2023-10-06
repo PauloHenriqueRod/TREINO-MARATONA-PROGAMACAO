416463b91d61ad8c4168c6fc792f28640aa585ed
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2023-10-06 09:26:23 -0300

    strStr python

---
+++ leet_cod/strStr/solution.py
@@ -0,0 +1,9 @@
+def strStr(haystack: str, needle: str) -> int:
+    tam = len(needle)
+    if haystack == needle:
+        return 0
+    for i in range(0, len(haystack) - tam + 1):
+        if haystack[i:tam+i] == needle:
+            return i
+    return - 1
+print(strStr("abc", "c"))
