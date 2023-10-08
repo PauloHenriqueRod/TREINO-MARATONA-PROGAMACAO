3998270098188b54eb7d27a39b1068eeda8bfd94
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2023-10-08 17:49:35 -0300

    Update lib.cpp

--- biblioteca/lib.cpp
+++ biblioteca/lib.cpp
@@ -6,3 +6,20 @@ int calculo_mmc(int a, int b){ /* Algoritmo de Euclides para cálculo de mmc O(l
     }
     return a;
 }
+
+bool eh_primo(int n){
+    if (n<=1)
+        return false;
+    if (n<=3)
+        return true;
+    if (n%2 == 0 || n%3 == 0)
+        return false;
+    int i = 5;
+    while (i*i<=n)
+    {
+        if (n % i == 0 || n % (i + 2) == 0)
+            return false;
+        i = i+6;
+    }
+    return true;
+}
