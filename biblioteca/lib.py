828c75fd3932dc31c489ddeabeb390a78510947f
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2023-10-08 17:38:26 -0300

    Update lib.py

--- biblioteca/lib.py
+++ biblioteca/lib.py
@@ -23,3 +23,17 @@ def calcular_mdc(numero1, numero2): # Algoritmo de Euclides
     while numero2:
         numero1, numero2 = numero2, numero1 % numero2
     return numero1
+
+def eh_primo(n): # Número primo ou não
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
