768ce43fdd8108457e03486907dd42b6ab166aac
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2023-10-08 17:38:23 -0300

    ex1221 python

---
+++ beecrowd/matematica/ex1221.py
@@ -0,0 +1,21 @@
+def eh_primo(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+n = int(input())
+for i in range(n):
+    num = int(input())
+    if eh_primo(num):
+        print('Prime')
+    else:
+        print('Not Prime')
