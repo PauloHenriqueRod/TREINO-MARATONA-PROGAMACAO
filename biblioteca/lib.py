7a1fdda1b5aaf2ea87097d2ef910d20cd30ee590
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2024-04-21 10:56:18 -0300

    add wagner fischer

--- biblioteca/lib.py
+++ biblioteca/lib.py
@@ -71,3 +71,27 @@ def circulo_inscrito(a,b,c,pi):
     areaT = ((p-a)*(p-b)*(p-c)*p)**(1/2)
     r = areaT/p
     return pi*(r**2)
+def wagner_fischer(str1, str2):
+    m = len(str1)
+    n = len(str2)
+    
+    # Inicializando a matriz de distância
+    dist = [[0] * (n + 1) for _ in range(m + 1)]
+    
+    # Inicializando a primeira coluna
+    for i in range(1, m + 1):
+        dist[i][0] = i
+    
+    # Inicializando a primeira linha
+    for j in range(1, n + 1):
+        dist[0][j] = j
+    
+    # Preenchendo a matriz de distância
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            cost = 0 if str1[i - 1] == str2[j - 1] else 1
+            dist[i][j] = min(dist[i - 1][j] + 1,      # Deleção
+                             dist[i][j - 1] + 1,      # Inserção
+                             dist[i - 1][j - 1] + cost)  # Substituição ou match
+    
+    return dist[m][n]
