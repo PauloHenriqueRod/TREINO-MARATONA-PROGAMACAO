6b90fbf3a1ee8b1c79223901e7b67b4e5288e1aa
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2024-05-20 10:35:06 -0300

    par_impar2.0

---
+++ beecrowd/matematica/ex1904.py
@@ -0,0 +1,44 @@
+def paridade(x):
+    n = 2
+    par = 0
+    while n <= x:
+        par += x // n
+        n*=2
+    return par
+
+def count_primos(n, m):
+    if not (n > m):
+        aux = n
+        n = m
+        m = aux
+    if n <= 1:
+        return 0
+
+    total = n-m
+    print(total)
+    prime = [1]*(n+1)
+    prime[0] = prime[1] = 0 # prime[0] = 0 and prime[1] = 1
+
+    for i in range(2,int(n**0.5) + 1):
+        if prime[i] == 1:
+            for j in range(i**2, n+1, i):
+                if prime[j] == 1:
+                    total-=1
+                prime[j] = 0
+    return total
+    
+n, m = [int(x) for x in input().split()]
+x = abs(n-m)
+if x==0:
+    print('?')
+else:
+    quant_primos = count_primos(n, m)
+    print(quant_primos)
+    if quant_primos == 0:
+        print("Bob")
+    else:
+        resposta = paridade(quant_primos+x-1) - (paridade(x)+paridade(quant_primos-1))
+        if resposta>0:
+            print('Bob')
+        else:
+            print('Alice')
