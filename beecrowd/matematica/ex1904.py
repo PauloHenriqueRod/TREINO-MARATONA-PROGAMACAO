63557ed01b67db61033a60139c1989b35faba63d
Paulo Henrique Rodrigues de Araujo <phrod.devp@gmail.com>
2024-05-20 15:35:32 -0300

    versão correta, parimpar

--- beecrowd/matematica/ex1904.py
+++ beecrowd/matematica/ex1904.py
@@ -13,17 +13,18 @@ def count_primos(n, m):
         m = aux
     if n <= 1:
         return 0
-
-    total = n-m
-    print(total)
+    total = n+1
     prime = [1]*(n+1)
     prime[0] = prime[1] = 0 # prime[0] = 0 and prime[1] = 1
+    total = total - 2
 
     for i in range(2,int(n**0.5) + 1):
         if prime[i] == 1:
+            if i < m:
+                total -= 1
             for j in range(i**2, n+1, i):
                 if prime[j] == 1:
-                    total-=1
+                    total -= 1
                 prime[j] = 0
     return total
     
@@ -33,7 +34,6 @@ if x==0:
     print('?')
 else:
     quant_primos = count_primos(n, m)
-    print(quant_primos)
     if quant_primos == 0:
         print("Bob")
     else:
